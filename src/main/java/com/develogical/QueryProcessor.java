cee190378269e74f8dbcd647c09939a6c77e6a77
ghusharib <ghusharib@hotmail.com>
2022-01-25 18:05:46 +0000

    Add query for ghush

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -12,6 +12,11 @@ public class QueryProcessor {
         if (query.toLowerCase().contains("imperial")) {
             return "Imperial College is a university in London";
         }
+
+        if (query.toLowerCase().contains("ghush")) {
+            return "A Fourth Year Computing Student at Imperial College London";
+        }
+
         return "";
     }
 }
